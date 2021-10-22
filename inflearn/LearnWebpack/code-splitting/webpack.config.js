1e25c3d4924f54c5c28f41e1d75b4e63e4a0ed36
wesbin <leegosoo11@naver.com>
2021-10-22 12:55:31 +0900

    webpack 끝!

--- inflearn/LearnWebpack/code-splitting/webpack.config.js
+++ inflearn/LearnWebpack/code-splitting/webpack.config.js
@@ -1,4 +1,25 @@
+// const path = require('path');
+//
+// module.exports = {
+//   mode: 'none',
+//   entry: './index.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist')
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader']
+//       }
+//     ]
+//   },
+// }
+
+// webpack.config.js
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'none',
@@ -11,8 +32,14 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader"
+        ]
       }
     ]
   },
+  plugins: [
+    new MiniCssExtractPlugin()
+  ],
 }
