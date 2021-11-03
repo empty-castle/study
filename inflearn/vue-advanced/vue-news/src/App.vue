f5f6d971ef512850101c766db622c172c1c4e907
wesbin <leegosoo11@naver.com>
2021-11-03 13:00:29 +0900

    스타일링 중

--- inflearn/vue-advanced/vue-news/src/App.vue
+++ inflearn/vue-advanced/vue-news/src/App.vue
@@ -23,9 +23,25 @@ body {
   padding: 0;
   margin: 0;
 }
+
+a {
+  color: #34495e;
+  text-decoration: none;
+}
+
+a.router-link-exact-active {
+  text-decoration: underline;
+}
+
+a:hover {
+  color: #42b883;
+  text-decoration: underline;
+}
+
 .page-enter-active, .page-leave-active {
   transition: opacity .5s;
 }
+
 .page-enter, .page-leave-to {
   opacity: 0;
 }
--- inflearn/vue-advanced/vue-news/src/views/NewsView.vue
+++ inflearn/vue-advanced/vue-news/src/views/NewsView.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
-    <p :key="item.id" v-for="item in fetchedNews">
-      <a :href="item.url">{{ item.title }}</a>
-      <small>{{ item.time_ago }} by {{ item.domain }}</small>
-    </p>
+    <ul class="news-list">
+      <li :key="item.id" v-for="item in fetchedNews" class="post">
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <div>
+          <p class="news-title">
+            <a :href="item.url">
+              {{ item.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            by
+            <router-link :to="`/user/${item.user}`">
+              {{ item.user }}
+            </router-link>
+          </small>
+        </div>
+<!--        <small>{{ item.time_ago }} by {{ item.domain }}</small>-->
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -22,5 +39,30 @@ export default {
 </script>
 
 <style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
 
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
 </style>
