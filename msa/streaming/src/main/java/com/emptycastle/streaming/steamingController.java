d19e0321c79ab60ec099fa99dc5dcb1679cda5c1
wesbin <leegosoo11@naver.com>
2022-01-26 19:55:58 +0900

    webclient testing

--- msa/streaming/src/main/java/com/emptycastle/streaming/steamingController.java
+++ msa/streaming/src/main/java/com/emptycastle/streaming/steamingController.java
@@ -2,9 +2,11 @@ package com.emptycastle.streaming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -15,12 +17,24 @@ public class steamingController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    WebClient webClient;
+
     @PostMapping("/play")
     public Mono<String> play(@RequestBody Map<String, String> body) {
-        return WebClient.create()
+        return webClient
+                .mutate()
+                .filter((request, next) -> next.exchange(
+                        ClientRequest.from(request).header("test", "test").build()
+                ))
+                .build()
                 .post()
                 .uri("http://localhost:8081/fetch")
                 .retrieve()
                 .bodyToMono(String.class);
+//        return webClient.post()
+//                .uri("http://localhost:8081/fetch")
+//                .retrieve()
+//                .bodyToMono(String.class);
     }
 }
--- msa/user/src/main/java/com/emptycastle/user/userController.java
+++ msa/user/src/main/java/com/emptycastle/user/userController.java
@@ -3,7 +3,10 @@ package com.emptycastle.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 
 @RestController
@@ -12,7 +15,7 @@ public class userController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @PostMapping("/fetch")
-    public String fetch() {
+    public String fetch(@RequestHeader Map<String, String> header) {
         logger.info("[USER] fetch");
         return "[USER] fetch";
     }
