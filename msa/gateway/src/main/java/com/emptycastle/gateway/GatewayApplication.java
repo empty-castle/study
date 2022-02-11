c6086b65ff9f4dccc77728f99b2e002922fc5d8f
wesbin <leegosoo11@naver.com>
2022-02-11 13:09:15 +0900

    msa docker? kubernetes?

--- msa/gateway/src/main/java/com/emptycastle/gateway/GatewayApplication.java
+++ msa/gateway/src/main/java/com/emptycastle/gateway/GatewayApplication.java
@@ -5,7 +5,6 @@ import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
@@ -20,22 +19,19 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-//    @Bean
-//    public Customizer<ReactiveResilience4JCircuitBreakerFactory> slowCustomizer() {
-//        return factory -> factory.configure(builder -> builder
-//                .circuitBreakerConfig(CircuitBreakerConfig.custom()
-//                        .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
-//                        .slidingWindowSize(10)
-//                        .minimumNumberOfCalls(2)
-//                        .failureRateThreshold(60)
-//                        .waitDurationInOpenState(Duration.ofSeconds(10))
-//                        .build()
-//                ).build(), "slowCustomizer");
-//    }
-
     @Bean
     public Customizer<ReactiveResilience4JCircuitBreakerFactory> slowCustomizer() {
-        return factory -> factory.configure(builder -> builder.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(1)).build()), "slow");
+        return factory -> factory.configure(builder -> builder
+                .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                        .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                        .slidingWindowSize(10)
+                        .minimumNumberOfCalls(2)
+                        .failureRateThreshold(60)
+                        .waitDurationInOpenState(Duration.ofSeconds(10))
+                        .build()
+                ).timeLimiterConfig(TimeLimiterConfig.custom()
+                        .timeoutDuration(Duration.ofSeconds(1))
+                        .build())
+                .build(), "slowCustomizer");
     }
 }
