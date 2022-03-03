6dcd44a3e8ea87f5a62ef6fc3516cc91d1a0fd09
wesbin <leegosoo11@naver.com>
2022-03-03 18:00:51 +0900

    plugin gogo

---
+++ intellij-plugin/src/main/java/ui/TestForm.java
@@ -0,0 +1,28 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+public class TestForm {
+    private JButton search;
+    private JPanel TestForm;
+    private JTextField inputText;
+    private JLabel label;
+
+    public TestForm() {
+        search.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println(inputText.getText());
+                JOptionPane.showMessageDialog(null, inputText.getText());
+                label.setText(inputText.getText());
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
