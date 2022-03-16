a9a7c48b6fedb1e1d072d7afc65e8baaa02977aa
wesbin <leegosoo11@naver.com>
2022-03-16 13:14:58 +0900

    kotlin이라니

---
+++ intellij_plugin/intellij-plugin/src/main/java/icons/DatabaseIcons.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package icons;
+
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class DatabaseIcons {
+  private static @NotNull Icon load(@NotNull String path, long cacheKey, int flags) {
+    return IconManager.getInstance().loadRasterizedIcon(path, DatabaseIcons.class.getClassLoader(), cacheKey, flags);
+  }
+  /** 16x16 */ public static final @NotNull Icon Access_method = load("icons/access_method.svg", 393229094207421121L, 0);
+  /** 16x16 */ public static final @NotNull Icon Adapter_script = load("icons/adapter_script.svg", 9138001896052997261L, 0);
+  /** 10x10 */ public static final @NotNull Icon AddHover = load("icons/add-hover.svg", 5336691655723985982L, 2);
+  /** 10x10 */ public static final @NotNull Icon Add = load("icons/add.svg", 7384713711841903640L, 2);
+  /** 16x16 */ public static final @NotNull Icon Aggregate = load("icons/aggregate.svg", 6579607116810718461L, 0);
+  /** 16x16 */ public static final @NotNull Icon Argument = load("icons/argument.svg", 1148848329148759805L, 0);
+  /** 16x16 */ public static final @NotNull Icon BinaryData = load("icons/binaryData.svg", 3995505741168572814L, 2);
+  /** 16x16 */ public static final @NotNull Icon BlueKey = load("icons/blueKey.svg", 3782605962318565746L, 0);
+  /** 16x16 */ public static final @NotNull Icon BlueKeyTrigger = load("icons/blueKeyTrigger.svg", -8028166109345987644L, 0);
+  /** 16x16 */ public static final @NotNull Icon Body = load("icons/body.svg", 6920145245172294729L, 0);
+  /** 16x16 */ public static final @NotNull Icon CheckConstraint = load("icons/checkConstraint.svg", 4335368663973642876L, 0);
+  /** 16x16 */ public static final @NotNull Icon Col = load("icons/col.svg", -9174807065800785443L, 0);
+  /** 16x16 */ public static final @NotNull Icon ColBlueKey = load("icons/colBlueKey.svg", 6348530867339803280L, 0);
+  /** 16x16 */ public static final @NotNull Icon ColBlueKeyDot = load("icons/colBlueKeyDot.svg", 8767742543376697869L, 2);
+  /** 16x16 */ public static final @NotNull Icon ColBlueKeyDotIndex = load("icons/colBlueKeyDotIndex.svg", 5532158407444015065L, 2);
+  /** 16x16 */ public static final @NotNull Icon ColBlueKeyIndex = load("icons/colBlueKeyIndex.svg", -1296422879443746651L, 0);
+  /** 16x16 */ public static final @NotNull Icon ColDot = load("icons/colDot.svg", -2149143735311120149L, 2);
+  /** 16x16 */ public static final @NotNull Icon ColDotIndex = load("icons/colDotIndex.svg", -6928123448154488031L, 2);
+  /** 16x16 */ public static final @NotNull Icon ColGoldBlueKey = load("icons/colGoldBlueKey.svg", -6386047073804885126L, 0);
+  /** 16x16 */ public static final @NotNull Icon ColGoldBlueKeyDot = load("icons/colGoldBlueKeyDot.svg", -9206551574475437287L, 2);
+  /** 16x16 */ public static final @NotNull Icon ColGoldBlueKeyDotIndex = load("icons/colGoldBlueKeyDotIndex.svg", 361505340104521572L, 2);
+  /** 16x16 */ public static final @NotNull Icon ColGoldBlueKeyIndex = load("icons/colGoldBlueKeyIndex.svg", 7136781684405336402L, 0);
+  /** 16x16 */ public static final @NotNull Icon ColGoldKey = load("icons/colGoldKey.svg", -8895941049878098589L, 0);
+  /** 16x16 */ public static final @NotNull Icon ColGoldKeyDot = load("icons/colGoldKeyDot.svg", -4292565929952918974L, 2);
+  /** 16x16 */ public static final @NotNull Icon ColGoldKeyDotIndex = load("icons/colGoldKeyDotIndex.svg", -8468478498839588375L, 2);
+  /** 16x16 */ public static final @NotNull Icon ColGoldKeyIndex = load("icons/colGoldKeyIndex.svg", -7111593285947674902L, 0);
+  /** 16x16 */ public static final @NotNull Icon ColGreyKey = load("icons/colGreyKey.svg", 6314022068697192915L, 0);
+  /** 16x16 */ public static final @NotNull Icon ColIndex = load("icons/colIndex.svg", -8935489616847421337L, 0);
+  /** 16x16 */ public static final @NotNull Icon Collation = load("icons/collation.svg", -1452858511928252794L, 0);
+  /** 16x16 */ public static final @NotNull Icon CollectionType = load("icons/collectionType.svg", 8908186652324387748L, 0);
+  /** 16x16 */ public static final @NotNull Icon Connector = load("icons/connector.svg", -7208536858477309534L, 0);
+  /** 16x16 */ public static final @NotNull Icon ConsoleRun = load("icons/consoleRun.svg", 4188553016426165569L, 2);
+  /** 16x16 */ public static final @NotNull Icon ConsoleShowPlan = load("icons/consoleShowPlan.svg", -3261031456791105737L, 2);
+  /** 16x16 */ public static final @NotNull Icon Constant = load("icons/constant.svg", -3410238681411247925L, 0);
+  /** 16x16 */ public static final @NotNull Icon Database = load("icons/database.svg", -2420828641667959307L, 0);
+  /** 16x16 */ public static final @NotNull Icon DatabaseLink = load("icons/databaseLink.svg", 4244327578828292943L, 2);
+  /** 16x16 */ public static final @NotNull Icon DatabaseObjGroup = load("icons/databaseObjGroup.svg", -7273130632702365231L, 0);
+  /** 16x16 */ public static final @NotNull Icon DataFile = load("icons/dataFile.svg", -6699699761897537452L, 0);
+  /** 16x16 */ public static final @NotNull Icon Dbms = load("icons/dbms.svg", 2791673484143529627L, 0);
+  /** 16x16 */ public static final @NotNull Icon DdlDbms = load("icons/ddlDbms.svg", 2294084964269794305L, 1);
+  /** 16x16 */ public static final @NotNull Icon DefaultConstraint = load("icons/defaultConstraint.svg", -967617992550929995L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverBlue1 = load("icons/driverBlue1.svg", -6221730219856466800L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverBlue2 = load("icons/driverBlue2.svg", -8473274734644375805L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverBlue3 = load("icons/driverBlue3.svg", 4852267363810505555L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverBlue4 = load("icons/driverBlue4.svg", 6947838465237631025L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverGreen1 = load("icons/driverGreen1.svg", 1773607936437849336L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverGreen2 = load("icons/driverGreen2.svg", 3628852149158267897L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverGreen3 = load("icons/driverGreen3.svg", -1510775756999787872L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverGreen4 = load("icons/driverGreen4.svg", 7935505544275683787L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverPink1 = load("icons/driverPink1.svg", 5564889611755467871L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverPink2 = load("icons/driverPink2.svg", -5313327620402825324L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverPink3 = load("icons/driverPink3.svg", 5389553809262574167L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverPink4 = load("icons/driverPink4.svg", 3257380787268307614L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverRed1 = load("icons/driverRed1.svg", -2178048274780970564L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverRed2 = load("icons/driverRed2.svg", -5234546976416152666L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverRed3 = load("icons/driverRed3.svg", -3234038664510580870L, 0);
+  /** 16x16 */ public static final @NotNull Icon DriverRed4 = load("icons/driverRed4.svg", 4958529991434759434L, 0);
+  /** 16x16 */ public static final @NotNull Icon EditorOutput = load("icons/editorOutput.svg", -1530075743900315276L, 2);
+  /** 16x16 */ public static final @NotNull Icon Extension = load("icons/extension.svg", -7701870837521923569L, 0);
+  /** 16x16 */ public static final @NotNull Icon External_schema_object = load("icons/external_schema_object.svg", 2490661642338003530L, 2);
+  /** 16x16 */ public static final @NotNull Icon FileFormat = load("icons/fileFormat.svg", -8431320570780060730L, 0);
+  /** 16x16 */ public static final @NotNull Icon FilterHistory = load("icons/filterHistory.svg", 538155257393359187L, 0);
+  /** 16x16 */ public static final @NotNull Icon Foreign_datawrapper = load("icons/foreign_datawrapper.svg", 2796196438717881523L, 2);
+  /** 16x16 */ public static final @NotNull Icon Foreign_server = load("icons/foreign_server.svg", -4881747320684241175L, 2);
+  /** 16x16 */ public static final @NotNull Icon Foreign_table = load("icons/foreign_table.svg", 7268574619489590204L, 2);
+  /** 16x16 */ public static final @NotNull Icon Function = load("icons/function.svg", 8847994053944924173L, 1);
+  /** 16x16 */ public static final @NotNull Icon FunctionRun = load("icons/functionRun.svg", -2145504619240857256L, 1);
+  /** 16x16 */ public static final @NotNull Icon GoldKey = load("icons/goldKey.svg", 7356670045363599994L, 0);
+  /** 16x16 */ public static final @NotNull Icon GoldKeyAlt = load("icons/goldKeyAlt.svg", -3963821819019506665L, 0);
+  /** 16x16 */ public static final @NotNull Icon GreenBugOverlap = load("icons/greenBugOverlap.svg", -443673311834405191L, 2);
+  /** 16x16 */ public static final @NotNull Icon HashCluster = load("icons/hashCluster.svg", -6707029994248433387L, 2);
+  /** 16x16 */ public static final @NotNull Icon Index = load("icons/index.svg", -802828522428326665L, 0);
+  /** 16x16 */ public static final @NotNull Icon IndexCluster = load("icons/indexCluster.svg", -5666077400209625528L, 0);
+  /** 16x16 */ public static final @NotNull Icon IndexFun = load("icons/indexFun.svg", -6076933452777908660L, 2);
+  /** 16x16 */ public static final @NotNull Icon IndexUnique = load("icons/indexUnique.svg", 857673827283026192L, 2);
+  /** 16x16 */ public static final @NotNull Icon IndexUniqueFun = load("icons/indexUniqueFun.svg", 61531630402739952L, 2);
+  /** 16x16 */ public static final @NotNull Icon Macro = load("icons/macro.svg", 9210506435770601301L, 1);
+  /** 16x16 */ public static final @NotNull Icon ManageDataSources = load("icons/manageDataSources.svg", 2943205061439077272L, 2);
+  /** 16x16 */ public static final @NotNull Icon MaterializedLog = load("icons/materializedLog.svg", 2158847163800881589L, 2);
+  /** 16x16 */ public static final @NotNull Icon MaterializedView = load("icons/materializedView.svg", -2195501725838677884L, 2);
+  /** 16x16 */ public static final @NotNull Icon MongoField = load("icons/mongoField.svg", -4440110326033946248L, 0);
+  /** 16x16 */ public static final @NotNull Icon MongoFieldGoldKey = load("icons/mongoFieldGoldKey.svg", -6135131038743141602L, 0);
+  /** 16x16 */ public static final @NotNull Icon ObjectGroup = load("icons/objectGroup.svg", -8549079339747751562L, 0);
+  /** 16x16 */ public static final @NotNull Icon Operator = load("icons/operator.svg", 1959831112866017898L, 0);
+  /** 16x16 */ public static final @NotNull Icon OperatorClass = load("icons/operatorClass.svg", -372697693324279684L, 0);
+  /** 16x16 */ public static final @NotNull Icon OperatorFamily = load("icons/operatorFamily.svg", -2797100599246827220L, 0);
+  /** 16x16 */ public static final @NotNull Icon Package = load("icons/package.svg", -8944222740281552899L, 0);
+  /** 16x16 */ public static final @NotNull Icon Partition = load("icons/partition.svg", -1398727355607698673L, 0);
+  /** 16x16 */ public static final @NotNull Icon PreviewChanges = load("icons/previewChanges.svg", 7570021985272644900L, 2);
+  /** 16x16 */ public static final @NotNull Icon Procedure = load("icons/procedure.svg", -81268963994333214L, 0);
+  /** 16x16 */ public static final @NotNull Icon ProcedureRun = load("icons/procedureRun.svg", -6997261868832358263L, 0);
+  /** 16x16 */ public static final @NotNull Icon ProcGroup = load("icons/procGroup.svg", -271888899709230381L, 1);
+  /** 16x16 */ public static final @NotNull Icon Projection = load("icons/projection.svg", 8230821542932576733L, 2);
+  /** 16x16 */ public static final @NotNull Icon Role = load("icons/role.svg", -2038204690845218411L, 0);
+  /** 16x16 */ public static final @NotNull Icon RollbackDB = load("icons/rollbackDB.svg", 907304357271158972L, 2);
+  /** 16x16 */ public static final @NotNull Icon Routine = load("icons/routine.svg", 2077813314172902736L, 0);
+  /** 16x16 */ public static final @NotNull Icon Rule = load("icons/rule.svg", -7678962973829446062L, 0);
+  /** 16x16 */ public static final @NotNull Icon RunDatabaseScript = load("icons/runDatabaseScript.svg", 344135359532871805L, 2);
+  /** 16x16 */ public static final @NotNull Icon ScheduledEvent = load("icons/scheduledEvent.svg", -6151627495381801627L, 0);
+  /** 16x16 */ public static final @NotNull Icon Schema = load("icons/schema.svg", -6805462646163629910L, 0);
+  /** 16x16 */ public static final @NotNull Icon Scripting_script = load("icons/scripting_script.svg", -7123190858159110032L, 2);
+  /** 16x16 */ public static final @NotNull Icon Sequence = load("icons/sequence.svg", 2956571468063393579L, 0);
+  /** 16x16 */ public static final @NotNull Icon ServerObjGroup = load("icons/serverObjGroup.svg", 2964121299581051822L, 0);
+  /** 16x16 */ public static final @NotNull Icon SortHistory = load("icons/sortHistory.svg", 838089623039377423L, 0);
+  /** 16x16 */ public static final @NotNull Icon Sql = load("icons/sql.svg", -5575629180676396383L, 0);
+  /** 16x16 */ public static final @NotNull Icon SqlDmlStatement = load("icons/sqlDmlStatement.svg", 7093455034393114310L, 0);
+  /** 16x16 */ public static final @NotNull Icon SqlGroupByType = load("icons/sqlGroupByType.svg", 4044955584908736295L, 2);
+  /** 16x16 */ public static final @NotNull Icon SqlOtherStatement = load("icons/sqlOtherStatement.svg", 6707532152295667861L, 0);
+  /** 16x16 */ public static final @NotNull Icon SqlSelectStatement = load("icons/sqlSelectStatement.svg", -4600957377425103462L, 0);
+  /** 16x16 */ public static final @NotNull Icon SubmitDB = load("icons/submitDB.svg", -3076425520941603791L, 2);
+  /** 16x16 */ public static final @NotNull Icon Synonym = load("icons/synonym.svg", -1356010689220336215L, 2);
+  /** 16x16 */ public static final @NotNull Icon Table = load("icons/table.svg", -5012632623981316672L, 0);
+  /** 16x16 */ public static final @NotNull Icon Tablespace = load("icons/tablespace.svg", -4286589075600922201L, 0);
+  /** 13x13 */ public static final @NotNull Icon ToolWindowConsole = load("icons/toolWindowConsole.svg", 6325373790545538002L, 2);
+  /** 13x13 */ public static final @NotNull Icon ToolWindowDatabase = load("icons/toolWindowDatabase.svg", -9134899490329948726L, 2);
+  /** 13x13 */ public static final @NotNull Icon ToolwindowDatabaseChanges = load("icons/toolwindowDatabaseChanges.svg", 8658478032618649867L, 2);
+  /** 13x13 */ public static final @NotNull Icon ToolWindowFiles = load("icons/toolWindowFiles.svg", 4086895966056902275L, 2);
+  /** 13x13 */ public static final @NotNull Icon ToolWindowSQLGenerator = load("icons/toolWindowSQLGenerator.svg", 6991981379783279486L, 2);
+  /** 16x16 */ public static final @NotNull Icon Trigger = load("icons/trigger.svg", -6205102712569671291L, 0);
+  /** 16x16 */ public static final @NotNull Icon TSQLt = load("icons/tSQLt.svg", -3196783758374594780L, 0);
+  /** 16x16 */ public static final @NotNull Icon Udf_script = load("icons/udf_script.svg", -3386543463424957322L, 2);
+  /** 16x16 */ public static final @NotNull Icon UnspecifiedCluster = load("icons/unspecifiedCluster.svg", -6406616648207398559L, 0);
+  /** 16x16 */ public static final @NotNull Icon User_mapping = load("icons/user_mapping.svg", -89947893612361666L, 0);
+  /** 16x16 */ public static final @NotNull Icon UserDriver = load("icons/userDriver.svg", -652802395433347802L, 0);
+  /** 16x16 */ public static final @NotNull Icon UtPLSQL = load("icons/utPLSQL.svg", -8059126629768831914L, 0);
+  /** 16x16 */ public static final @NotNull Icon Warehouse = load("icons/warehouse.svg", 8798902542112337670L, 0);
+}
