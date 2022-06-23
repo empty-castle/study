8912ff6bace7aeb92b9521d2d24e224fcf66ddad
wesbin <leegosoo11@naver.com>
2022-06-23 11:24:12 +0900

    test code upload

--- kotlin/jpa/src/main/kotlin/com/emptycastle/jpa/JpaApplication.kt
+++ kotlin/jpa/src/main/kotlin/com/emptycastle/jpa/JpaApplication.kt
@@ -1,10 +1,19 @@
 package com.emptycastle.jpa
 
+import com.emptycastle.jpa.repository.CustomerRepo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
+//@Component
 @SpringBootApplication
 class JpaApplication
+
+//class JpaApplication (private val customerRepo: CustomerRepo) {
+//	init {
+//		println(customerRepo.findAllBy())
+//	}
+//}
 
 fun main(args: Array<String>) {
 	runApplication<JpaApplication>(*args)
