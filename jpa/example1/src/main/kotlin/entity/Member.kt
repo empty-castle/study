bb18a1c5feebb7d456642773aa9108d2775c4946
wesbin <leegosoo11@naver.com>
2022-11-28 15:03:59 +0900

    JPA 11.28. update #1

--- jpa/example1/src/main/kotlin/entity/Member.kt
+++ jpa/example1/src/main/kotlin/entity/Member.kt
@@ -1,10 +1,9 @@
 package entity
 
+import RoleType.RoleType
 import org.hibernate.annotations.DynamicUpdate
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
+import java.util.Date
+import javax.persistence.*
 
 @Entity
 //@DynamicUpdate => 수정된 값만 수정 쿼리에 포함, 컬럼 30개 이상인 경우에만 사용, 자매품 DynamicInsert
@@ -19,4 +18,16 @@ class Member {
     var username: String? = null
 
     var age: Int? = null
+
+    @Enumerated(EnumType.STRING)
+    var roleType: RoleType? = null
+
+    @Temporal(TemporalType.TIMESTAMP)
+    var createdDate: Date? = null
+
+    @Temporal(TemporalType.TIMESTAMP)
+    var lastModifiedDate: Date? = null
+
+    @Lob
+    var description: String? = null
 }
