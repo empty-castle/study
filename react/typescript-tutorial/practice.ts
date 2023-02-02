4ae3f2cf91a790e229b1bf8226ae87ee8d5cf143
wesbin <leegosoo11@naver.com>
2023-02-02 16:58:35 +0900

    velopert tutorial start

---
+++ react/typescript-tutorial/practice.ts
@@ -0,0 +1,14 @@
+const numbers: number[] = [1,2,34]
+
+const strings: string[] = ['a', 'b']
+
+let color: 'red' | 'blue' | 'white' = 'red'
+
+function sum(x: number, y: number): number {
+  return x + y
+}
+
+function returnNothing(): void {
+  console.log('I am just saying hello world');
+}
+
