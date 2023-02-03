d27b5832e377649e91387a923f7acdb18ec464b1
wesbin <leegosoo11@naver.com>
2023-02-03 13:53:22 +0900

    typescript study end

--- react/velopert/typescript-tutorial/practice.ts
+++ react/velopert/typescript-tutorial/practice.ts
@@ -159,26 +159,40 @@ const wrapped = wrap(10)
 
 // Generics with interface
 
+interface Items<T> {
+  list: T[];
+}
 
+const items: Items<string> = {
+  list: ['a', 'b', 'c']
+}
 
+// Generics with Class
 
+class Queue<T> {
+  list: T[] = [];
 
+  get length() {
+    return this.list.length;
+  }
 
+  enqueue(item: T) {
+    this.list.push(item)
+  }
 
+  dequeue() {
+    return this.list.shift()
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const queue = new Queue<number>();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
