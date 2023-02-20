5c0058f5a9a69e862f366a1a903ce5ce7a423d24
wesbin <leegosoo11@naver.com>
2023-02-20 15:04:15 +0900

    #7.1 To Do List part Two

--- react/nomadcoder/react-for-beginners/src/App.js
+++ react/nomadcoder/react-for-beginners/src/App.js
@@ -1,22 +1,33 @@
-import {useState, useEffect} from 'react';
-
-function Hello() {
-  useEffect(() => {
-    console.log('hi :)')
-    return () => console.log('bye :(')
-  }, [])
-  return (
-    <h1>Hello</h1>
-  )
-}
+import {useState} from 'react';
 
 function App() {
-  const [showing, setShowing] = useState(false)
-  const onClick = () => setShowing(prev => !prev)
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos(prev => [toDo, ...prev]);
+    setToDo('');
+  };
   return (
     <div className="App">
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type={'text'}
+          placeholder={'Write your to do...'}
+          value={toDo}
+          onChange={onChange}
+        />
+        <button>Add To Do</button>
+        <hr/>
+        <ul>
+          {toDos.map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
+      </form>
     </div>
   );
 }
