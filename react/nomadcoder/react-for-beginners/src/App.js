336541a4c9ca912573a404cf789a3832d47dbbfb
wesbin <leegosoo11@naver.com>
2023-02-20 14:03:03 +0900

    #6.4 useEffect clean function

--- react/nomadcoder/react-for-beginners/src/App.js
+++ react/nomadcoder/react-for-beginners/src/App.js
@@ -1,33 +1,22 @@
-import styles from './App.module.css';
 import {useState, useEffect} from 'react';
 
+function Hello() {
+  useEffect(() => {
+    console.log('hi :)')
+    return () => console.log('bye :(')
+  }, [])
+  return (
+    <h1>Hello</h1>
+  )
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log('i run all the time');
-  useEffect(() => {
-    console.log('CALL THE API...');
-  }, []);
-  useEffect(() => {
-    if (keyword !== '' && keyword.length > 5) {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword])
-  useEffect(() => {
-    console.log("i run when 'counter' changes");
-  }, [counter])
+  const [showing, setShowing] = useState(false)
+  const onClick = () => setShowing(prev => !prev)
   return (
     <div className="App">
-      <input
-        type={'text'}
-        placeholder={'Search here...'}
-        value={keyword}
-        onChange={onChange}
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
