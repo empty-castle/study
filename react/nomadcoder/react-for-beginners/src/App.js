481474d6e4b164a88ec2021095c7486c48227cb0
wesbin <leegosoo11@naver.com>
2023-02-20 13:46:15 +0900

    #6.2 useEffect deps

--- react/nomadcoder/react-for-beginners/src/App.js
+++ react/nomadcoder/react-for-beginners/src/App.js
@@ -3,13 +3,29 @@ import {useState, useEffect} from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log('i run all the time');
   useEffect(() => {
     console.log('CALL THE API...');
   }, []);
+  useEffect(() => {
+    if (keyword !== '' && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword])
+  useEffect(() => {
+    console.log("i run when 'counter' changes");
+  }, [counter])
   return (
     <div className="App">
+      <input
+        type={'text'}
+        placeholder={'Search here...'}
+        value={keyword}
+        onChange={onChange}
+      />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
